35c30ac5dd58b518c279e81bc8ac5af2d1ff5301
vitlobo <awhite1196@proton.me>
2025-07-28 15:19:37 -0400

    Final project submission - Now requires sys args

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from stats import (
     sort_dict,
 )
 
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_count = get_times_used(text.lower())
-    c_sorted_list = sort_dict(get_times_used(text.lower()))
+    c_sorted_list = sort_dict(character_count)
     print_report(book_path, num_words, c_sorted_list)
 
 
